9d1048c3792e85fc2d503089d22bebc2da61e356
Kharel-ranjan11 <aryarae.0098@gmail.com>
2023-04-01 11:53:14 +0545

    this is 1 commit

--- src/App.js
+++ src/App.js
@@ -1,45 +1,77 @@
+import ReactPlayer from 'react-player/youtube'
 import CreatableSelect from 'react-select/creatable'
 import './App.css'
+import { useState } from 'react'
 const App = () => {
+  const [index, setIndex] = useState(0)
   const options = [
     { value: 'jack', label: 'Jack', color: '#FF8B00' },
     { value: 'john', label: 'John', color: '#36B37E' },
-    { value: 'mike', label: 'Mike', color: '#0052CC' }
+    { value: 'a', label: 'a', color: '#0052CC' },
+    { value: 'b', label: 'b', color: '#0052CC' },
+    { value: 'c', label: 'c', color: '#0052CC' },
+    { value: 'd', label: 'd', color: '#0052CC' },
+    { value: 'e', label: 'e', color: '#0052CC' },
+    { value: 'f', label: 'f', color: '#0052CC' },
+    { value: 'g', label: 'g', color: 'silver' }
   ]
   const colorStyles = {
     control: styles => ({
       ...styles,
+      // =====================================================================
+      backgroundColor: 'red', //added
+      opacity: '0.4',
+
+      // =====================================================================
       borderColor: styles.isFocused ? 'red' : 'red',
-      boxShadow: styles.isFocused ? '0 0 20px transparent' : '0 0 2px transparent',
-      // borderColor: '#f21d3d',
+      boxShadow: styles.isFocused ? '0 0 0 transparent' : '0 0 0 transparent',
       '&:hover': {
         borderColor: 'red'
-        // boxShadow: styles.isFocused ? '0 0 0 red' : '0 0 2px red'
       }
     }),
     option: (styles, { data, isDisabled, isFocused, isSelected }) => {
-      return { ...styles, color: data.color }
+      console.log(data)
+      // ==========================================================================
+      return {
+        ...styles,
+        color: 'green',
+        // color: isFocused ? 'green' : 'blue', <----- use if u want
+        '&:hover': {
+          backgroundColor: 'red'
+        },
+        backgroundColor: isSelected ? 'white' : 'white'
+        // backgroundColor: 'yellow',
+      }
+      // ==========================================================================
     },
     multiValue: (styles, { data }) => {
       return {
         ...styles,
-        backgroundColor: data.color,
-        color: '#fff'
+        backgroundColor: 'green',
+        color: '#fff',
+        ':hover': {
+          color: 'red',
+          opacity: 0.4
+        }
       }
     },
     multiValueLabel: styles => {
       return {
         ...styles,
-        color: '#fff'
+        color: 'yellow',
+        ':hover': {
+          color: 'red',
+          opacity: 0.4
+        }
       }
     },
     multiValueRemove: styles => {
       return {
         ...styles,
-        color: '#fff',
+        color: 'blue',
         cursor: 'pointer',
         ':hover': {
-          color: '#fff'
+          color: 'red'
         }
       }
     }
@@ -50,7 +82,7 @@ const App = () => {
   const handleInputChange = (inputValue, actionMeta) => {
     // console.log('handleInputChange', inputValue, actionMeta)
   }
-  console.log(colorStyles)
+  // console.log(colorStyles)
   return (
     <>
       <div style={{ width: '300px', margin: '3rem' }}>
@@ -64,6 +96,67 @@ const App = () => {
         />
       </div>
       <br /> <br />
+      
+      <section>
+        <div className='tabs'>
+          <div className='tabList'>
+            <div
+              className={index === 0 ? 'tab activeTab' : 'tab inactiveTab'}
+              onClick={() => setIndex(0)}
+            >
+              Tab One
+            </div>
+            <div
+              className={index === 1 ? 'tab activeTab' : 'tab inactiveTab'}
+              onClick={() => setIndex(1)}
+            >
+              Tab Two
+            </div>
+            <div
+              className={index === 2 ? 'tab activeTab' : 'tab inactiveTab'}
+              onClick={() => setIndex(2)}
+            >
+              Tab Three
+            </div>
+          </div>
+          <div
+            className={index === 0 ? 'tabPanel inactive' : 'tabPanel active'}
+          >
+            <div>
+              <h3>About Computer</h3>A computer is a machine that can be
+              programmed to carry out sequences of arithmetic or logical
+              operations (computation) automatically. Modern digital electronic
+              computers can perform generic sets of operations known as
+              programs. These programs enable computers to perform a wide range
+              of tasks.
+            </div>
+          </div>
+          <div
+            className={index === 1 ? 'tabPanel inactive' : 'tabPanel active'}
+          >
+            <h3>About hardware</h3>
+            <div>
+              Hardware refers to the external and internal devices and equipment
+              that enable you to perform major functions such as input, output,
+              storage, communication, processing, and more. There are two types
+              of computer hardware: external and internal.
+            </div>
+          </div>
+          <div
+            className={index === 2 ? 'tabPanel inactive' : 'tabPanel active'}
+          >
+            <div>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet
+              deleniti placeat corporis nobis aliquam qui voluptates cum hic ad
+              blanditiis eius debitis quod rerum quidem consequuntur, quam
+              excepturi quaerat eos!
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className='player'>
+        <ReactPlayer url='https://www.youtube.com/watch?v=hp_-RlwNg04' volume={0.5} />
+      </div>
     </>
   )
 }
